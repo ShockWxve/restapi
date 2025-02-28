702f217481b5fe5d1333288b44bf8f49bdf23d20
Your Name <your.email@example.com>
2025-02-28 20:45:52 +0300

    кодогенерация get, post

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"log"
-	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/shock_wave/restapi/internal/database"
 	"github.com/shock_wave/restapi/internal/handlers"
 	"github.com/shock_wave/restapi/internal/taskService"
+	"github.com/shock_wave/restapi/internal/web/tasks"
 )
 
 func main() {
@@ -20,11 +21,26 @@ func main() {
 
 	handler := handlers.NewHandler(service)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/api/get", handler.GetTasksHandler).Methods("GET")
-	router.HandleFunc("/api/post", handler.PostTaskHandler).Methods("POST")
-	router.HandleFunc("/api/patch/{id}", handler.PatchTaskHandler).Methods("PATCH")
-	router.HandleFunc("/api/delete/{id}", handler.DeleteTaskHandler).Methods("DELETE")
-	log.Println("Сервер запущен")
-	http.ListenAndServe(":8080", router)
+	// router := mux.NewRouter()
+	// router.HandleFunc("/api/get", handler.GetTasksHandler).Methods("GET")
+	// router.HandleFunc("/api/post", handler.PostTaskHandler).Methods("POST")
+	// router.HandleFunc("/api/patch/{id}", handler.PatchTaskHandler).Methods("PATCH")
+	// router.HandleFunc("/api/delete/{id}", handler.DeleteTaskHandler).Methods("DELETE")
+	// log.Println("Сервер запущен")
+	// http.ListenAndServe(":8080", router)
+
+	// Инициализируем echo
+	e := echo.New()
+
+	// используем Logger и Recover
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Прикол для работы в echo. Передаем и регистрируем хендлер в echo
+	strictHandler := tasks.NewStrictHandler(handler, nil) // тут будет ошибка
+	tasks.RegisterHandlers(e, strictHandler)
+
+	if err := e.Start(":8080"); err != nil {
+		log.Fatalf("failed to start with err: %v", err)
+	}
 }
--- internal/handlers/taskHandlers.go
+++ internal/handlers/taskHandlers.go
@@ -1,13 +1,11 @@
 package handlers
 
 import (
-	"encoding/json"
+	"context"
 	"log"
-	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/shock_wave/restapi/internal/taskService"
+	"github.com/shock_wave/restapi/internal/web/tasks"
 )
 
 type Handler struct {
@@ -20,134 +18,164 @@ func NewHandler(service *taskService.TaskService) *Handler {
 	}
 }
 
-func (h *Handler) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+// GetTasks implements tasks.StrictServerInterface.
+func (h *Handler) GetTasks(ctx context.Context, request tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
 	log.Println("GET request")
 
-	tasks, err := h.Service.ReadAllTasks()
+	allTasks, err := h.Service.ReadAllTasks()
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil, err
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
+	// Создаем переменную респон типа 200джейсонРеспонс
+	// Которую мы потом передадим в качестве ответа
+	response := tasks.GetTasks200JSONResponse{}
+
+	// Заполняем слайс response всеми задачами из БД
+	for _, tsk := range allTasks {
+		task := tasks.Task{
+			Id:     &tsk.ID,
+			Task:   &tsk.Task,
+			IsDone: &tsk.IsDone,
+		}
+
+		response = append(response, task)
+	}
+
+	return response, nil
 }
 
-func (h *Handler) PostTaskHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("POST request")
-
-	var task taskService.Task
-
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&task); err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, e, http.StatusBadRequest)
-		return
-	}
-
-	createdTask, err := h.Service.CreateTask(task)
-	if err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, e, http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(createdTask)
+// PostTasks implements tasks.StrictServerInterface.
+func (h *Handler) PostTasks(ctx context.Context, request tasks.PostTasksRequestObject) (tasks.PostTasksResponseObject, error) {
+	panic("unimplemented")
 }
 
-func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("PATCH request")
-
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-
-	id, err := strconv.ParseUint(idStr, 10, strconv.IntSize)
-	if err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, "invalid id", http.StatusBadRequest)
-		return
-	}
-	taskID := uint(id)
-
-	var updateData struct {
-		Task   string `json:"task,omitempty"`
-		IsDone *bool  `json:"is_done,omitempty"`
-	}
-
-	// Декодируем тело запроса в структуру updateData
-	dec := json.NewDecoder(r.Body)
-	if err := dec.Decode(&updateData); err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, e, http.StatusBadRequest)
-		return
-	}
-
-	updates := make(map[string]interface{})
-	if updateData.Task != "" {
-		updates["task"] = updateData.Task
-	}
-	if updateData.IsDone != nil {
-		updates["is_done"] = *updateData.IsDone
-	}
-
-	// Проверяем, есть ли изменения
-	if len(updates) == 0 {
-		e := "empty fields"
-		log.Println(e)
-		http.Error(w, e, http.StatusBadRequest)
-		return
-	}
-
-	updatedTask, err := h.Service.UpdateTaskByID(taskID, updates)
-	if err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, e, http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(updatedTask)
+// GetTasks implements tasks.StrictServerInterface.
+func (h *Handler) PatchTasks(ctx context.Context, request tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
+	panic("unimplemented")
 }
 
-func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("DELETE request")
-
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-
-	id, err := strconv.ParseUint(idStr, 10, strconv.IntSize)
-	if err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, "invalid id", http.StatusBadRequest)
-		return
-	}
-	taskID := uint(id)
-
-	// var deleteData struct {
-	// 	ID uint `json:"id"`
-	// }
-
-	// dec := json.NewDecoder(r.Body)
-	// if err := dec.Decode(&deleteData); err != nil {
-	// 	e := err.Error()
-	// 	log.Println(e)
-	// 	http.Error(w, e, http.StatusBadRequest)
-	// 	return
-	// }
-
-	err = h.Service.DeleteTaskByID(taskID)
-	if err != nil {
-		e := err.Error()
-		log.Println(e)
-		http.Error(w, e, http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusNoContent)
+// GetTasks implements tasks.StrictServerInterface.
+func (h *Handler) DeleteTasks(ctx context.Context, request tasks.GetTasksRequestObject) (tasks.GetTasksResponseObject, error) {
+	panic("unimplemented")
 }
+
+// func (h *Handler) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("GET request")
+
+// 	tasks, err := h.Service.ReadAllTasks()
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(tasks)
+// }
+
+// func (h *Handler) PostTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("POST request")
+
+// 	var task taskService.Task
+
+// 	dec := json.NewDecoder(r.Body)
+// 	if err := dec.Decode(&task); err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	createdTask, err := h.Service.CreateTask(task)
+// 	if err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(createdTask)
+// }
+
+// func (h *Handler) PatchTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("PATCH request")
+
+// 	vars := mux.Vars(r)
+// 	idStr := vars["id"]
+
+// 	id, err := strconv.ParseUint(idStr, 10, strconv.IntSize)
+// 	if err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, "invalid id", http.StatusBadRequest)
+// 		return
+// 	}
+// 	taskID := uint(id)
+
+// 	var updateData struct {
+// 		Task   string `json:"task,omitempty"`
+// 		IsDone *bool  `json:"is_done,omitempty"`
+// 	}
+
+// 	// Декодируем тело запроса в структуру updateData
+// 	dec := json.NewDecoder(r.Body)
+// 	if err := dec.Decode(&updateData); err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	updates := make(map[string]interface{})
+// 	if updateData.Task != "" {
+// 		updates["task"] = updateData.Task
+// 	}
+// 	if updateData.IsDone != nil {
+// 		updates["is_done"] = *updateData.IsDone
+// 	}
+
+// 	// Проверяем, есть ли изменения
+// 	if len(updates) == 0 {
+// 		e := "empty fields"
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	updatedTask, err := h.Service.UpdateTaskByID(taskID, updates)
+// 	if err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(updatedTask)
+// }
+
+// func (h *Handler) DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("DELETE request")
+
+// 	vars := mux.Vars(r)
+// 	idStr := vars["id"]
+
+// 	id, err := strconv.ParseUint(idStr, 10, strconv.IntSize)
+// 	if err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, "invalid id", http.StatusBadRequest)
+// 		return
+// 	}
+// 	taskID := uint(id)
+
+// 	err = h.Service.DeleteTaskByID(taskID)
+// 	if err != nil {
+// 		e := err.Error()
+// 		log.Println(e)
+// 		http.Error(w, e, http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	w.WriteHeader(http.StatusNoContent)
+// }
